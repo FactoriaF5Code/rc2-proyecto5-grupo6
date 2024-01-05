c2b69b60d870887faf0d782f07fe39728d431cf0
herminiocastro <hxssport@gmail.com>
2024-01-05 14:03:39 +0100

    header con estilos

---
+++ src/Header/Header.jsx
@@ -0,0 +1,18 @@
+import "./Header.css";
+export const Header = () => {
+  return (
+    <header>
+      <div className="contenedor">
+        <h2 className="logotipo">Restflix</h2>
+        <nav>
+          <a href="#" className="activo">Inicio</a>
+          <a href="#">Programas</a>
+          <a href="#">Peliculas</a>
+          <a href="#">Novedades</a>
+          <a href="#">Mi Lista</a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+export default Header
