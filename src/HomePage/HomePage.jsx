9113ebf5ba1a3eae1900f6d16aeb60eb6a1e3ad4
herminiocastro <hxssport@gmail.com>
2024-01-09 13:46:33 +0100

    modificacion Homepage

--- src/HomePage/HomePage.jsx
+++ src/HomePage/HomePage.jsx
@@ -1,21 +1,74 @@
-import './HomePage.css'
+import { useState, useEffect } from "react";
+import "./HomePage.css";
 
-export const HomePage = () => {
-    return <main>
-        <div className="pelicula-principal">
-            <div className="contenedor">
-                <h3 className="Titulo">Titulo</h3>
-                <p className="sinopsis">Lorem ipsum dolor sit amet consectetur,
-                    adipisicing elit. Ab aliquam dnfskfns nfsdlkflw.sñlsdkfiusto enim itaque
-                    nisikskdjflakfalkfñsdlfwk fskdjkfajf  jflaksjfwlef  culpa quod incidunt t
-                </p>
-                <button role="button" className="boton"><i className="fa-solid fa-play"></i>Reproducir</button>
-                <button role="button" className="boton"><i className="fa-light fa-circle-info"></i>Mas información</button>
-            </div>
+const HomePage = () => {
+  const [movies, setMovies] = useState([]);
+  const [movieIndex, setMovieIndex] = useState(0);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const options = {
+          method: "GET",
+          headers: {
+            accept: "application/json",
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwNmJmZjUzNzhmZmJiNGVjMjBlYTkwZjU0NTI4NDljNiIsInN1YiI6IjY1OTNlYzdjMDI4ZjE0NzY5NGM3MDgxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.24AzQEsONnyfFPb-mLCCWn6KxN_5s9UOjEg7F2w5rAY",
+          },
+        };
+
+        const response = await fetch(
+          "https://api.themoviedb.org/3/movie/popular",
+          options
+        );
+        const data = await response.json();
+
+        if (data && data.results && data.results.length > 0) {
+          console.log(data.results);
+          setMovies(data.results);
+        }
+      } catch (error) {
+        console.error("Error al obtener películas:", error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setMovieIndex((prevIndex) => (prevIndex + 1) % (movies.length || 1));
+    }, 6000);
+
+    return () => clearInterval(interval);
+  }, [movies]);
+
+  const currentMovie = movies[movieIndex];
+
+  return (
+    <main>
+      <div className="pelicula-principal">
+        <div className="contenedor">
+          {currentMovie && (
+            <>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${currentMovie.backdrop_path}`}
+                alt={currentMovie.title}
+              />
+              <h3 className="Titulo">{currentMovie.title}</h3>
+              <p className="sinopsis">{currentMovie.overview}</p>
+            </>
+          )}
+          <button role="button" className="boton">
+            <i className="fa-solid fa-play"></i>Reproducir
+          </button>
+          <button role="button" className="boton">
+            <i className="fa-light fa-circle-info"></i>Más información
+          </button>
         </div>
+      </div>
+    </main>
+  );
+};
 
-    
-</main> 
-}
-export  default HomePage
-
+export default HomePage;
